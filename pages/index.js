a65b8b777c731e44b6b70976e652bef959357279
Yehuda Lehrfield <yll2113@columbia.edu>
2022-07-06 22:51:24 -0400

    hero and footer banners, added some products

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import { ClientError } from '@sanity/client'
 import React from 'react'
-
 import {Product, FooterBanner, HeroBanner} from '../components'
-const Home = () => {
+import { client } from '../lib/client'
+
+const Home = ({products,bannerData}) => {
   return (
     <>
-      <HeroBanner/>
+      <HeroBanner heroBanner = {bannerData.length && bannerData[1]}/>
     
       <div className = "products-heading">
         <h2>Best Sellers</h2>
@@ -12,13 +14,25 @@ const Home = () => {
       </div>
 
       <div className = "products-container">
-        {['Product 1', 'Product 2'].map(
-          (product) => product)}
+        {products?.map(
+          (product) => <Product key={product._id} product = {product} />)}
       </div>
 
-      <FooterBanner/>
+      <FooterBanner footerBanner = {bannerData && bannerData[0]}/>
     </>
   )
 }
 
+export const getServerSideProps = async () => {
+  // get all products from sanity
+  const query = '*[_type == "product"]';
+  const products = await client.fetch(query);
+  // get all banners
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: {products, bannerData}
+  }
+}
 export default Home
